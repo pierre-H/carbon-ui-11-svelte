7022df9d5660567540606ef94fc891c6e8f01829
Pirnay <pirnay@outlook.com>
2023-07-16 12:40:17 +0200

    replace index content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-	import { Accordion, AccordionItem } from '$lib';
-</script>
-
-<Accordion>
-	<AccordionItem>
-		<span slot="title">TITLE</span>
-		Coucou
-	</AccordionItem>
-	<AccordionItem title="Coucou B">Hello</AccordionItem>
-</Accordion>
+<p>Docs TODO</p>
